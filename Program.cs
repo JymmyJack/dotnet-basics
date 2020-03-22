035c8feb148df43ca35e44b8f2aaf12473940d5b
Jymmy J. Torres <jackmishima@hotmail.com>
2020-03-22 11:42:09 -0500

    Ejercicios basicos

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace netsamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo desde netcore");
+            System.Console.WriteLine("veneno sound");
+        }
+    }
+}
